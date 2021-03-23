c25c78fd6e9118e26d052150d907ca4f7319cfb8
Samed MUSABAŞOĞLU <samedmusabasoglu@gmail.com>
2021-03-23 22:46:25 +0300

    230421

--- SiteIcerik/Program.cs
+++ SiteIcerik/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Net;
 using HtmlAgilityPack;
 using StopWord;
-using Iveonik.Stemmers;
+using System.Linq;
 
 namespace SiteIcerik
 {
@@ -16,97 +16,107 @@ namespace SiteIcerik
 
             WebClient client = new WebClient();
 
-            string downloadString = client.DownloadString("http://www.stackoverflow.com");//arametre olarak gelcek 
+            string downloadString = client.DownloadString("http://www.stackoverflow.com");//parametre olarak gelcek 
 
             byte[] bytes = Encoding.Default.GetBytes(downloadString);
             downloadString = Encoding.UTF8.GetString(bytes);
 
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(downloadString);
-            //Console.WriteLine(htmlDoc);
+       
 
-            //foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//p//text()"))
-            //{
-            //    if (!string.IsNullOrEmpty(node.InnerText) && !string.IsNullOrWhiteSpace(node.InnerText))
-            //        Console.WriteLine("text=" + node.InnerText);
-
-            //}
-
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+     
 
             var stopWords = StopWords.GetStopWords("en");
 
 
-            //foreach (var item in stopWords)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //string cumle = "i write a lot of things for my example sentence in here for you ";
-
-            //string cumle2 = cumle.RemoveStopWords("en");
-            //Console.WriteLine(cumle);
-            //Console.WriteLine(cumle2);
+            
 
             List<string> kelimeler = new List<string>();
-            //kelimeler.Add("jumper");
-            //kelimeler.Add("jumped");
-            //kelimeler.Add("jumping");
-            //kelimeler.Add("writing");
-            //kelimeler.Add("wrote");
+
+
+            NetSpell.SpellChecker.Dictionary.WordDictionary oDict = new NetSpell.SpellChecker.Dictionary.WordDictionary();
+
+            oDict.DictionaryFile = "en-US.dic";
+            //load and initialize the dictionary 
+            oDict.Initialize();
+           
+            NetSpell.SpellChecker.Spelling oSpell = new NetSpell.SpellChecker.Spelling();
+
+            oSpell.Dictionary = oDict;
 
 
 
 
             foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//p//text()"))
             {
-                //if (!string.IsNullOrEmpty(node.InnerText) && !string.IsNullOrWhiteSpace(node.InnerText))
-                //    Console.WriteLine("text=" + node.InnerText);
+
 
                 string[] cumleKelime = node.InnerText.Split(' ');
                 foreach (var item in cumleKelime)
                 {
                     if (!string.IsNullOrEmpty(item) && !string.IsNullOrWhiteSpace(item))
                     {
-                        kelimeler.Add(item);
+
+                        kelimeler.Add(item.ToLower());
 
                     }
 
-                   
+
                 }
 
             }
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------BUTUN KELİMELER---------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+
+
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//title//text()"))
+            {
+
+
+                string[] cumleKelime = node.InnerText.Split(' ');
+                foreach (var item in cumleKelime)
+                {
+                    if (!string.IsNullOrEmpty(item) && !string.IsNullOrWhiteSpace(item))
+                    {
+                        kelimeler.Add(item.ToLower());
+                    }
+
+
+                }
+
+            }
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//meta"))
+            {
+
+                string icerik = node.GetAttributeValue("content", "");
+                string[] cumleKelime = icerik.Split(' ');
+
+                foreach (var item in cumleKelime)
+                {
+                    if (!string.IsNullOrEmpty(item) && !string.IsNullOrWhiteSpace(item))
+                    {
+                        
+                        kelimeler.Add(item.ToLower());
+
+                    }
+
+
+                }
+
+            }
+
+          
             for (int j = 0; j < kelimeler.Count; j++)
             {
 
                 for (int i = 0; i < kelimeler[j].Length; i++)
                 {
                     if (Char.IsPunctuation(kelimeler[j][i]))
-                      kelimeler[j]=  kelimeler[j].Remove(i,1);
+                        kelimeler[j] = kelimeler[j].Remove(i, 1);
 
                 }
-                Console.WriteLine(kelimeler[j]+"AA");
+               
             }
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-------------------------STOP WORD SİLİNMİŞ---------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+          
 
             for (int i = 0; i < kelimeler.Count; i++)
             {
@@ -137,86 +147,65 @@ namespace SiteIcerik
             }
 
 
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("---------TEMİZLENMİŞ ----------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
-            Console.WriteLine("-------------------------TEMİZLENMiş-----------------------------------------------");
+            List<WordAndFreq> KelimeSayilari = new List<WordAndFreq>();
+            int sayi = 0;
 
+            List<string> kelimelerSon = new List<string>();
 
-            foreach (var item in kelimeler)
+            foreach (string item in kelimeler)
             {
+                
+                if (oSpell.TestWord(item))
+                {
+                    //Console.WriteLine(item);
+                    sayi++;
+                    kelimelerSon.Add(item);
+                   
 
+
+                }
+
+                
+            }
+            Console.WriteLine(kelimeler.Count);
+            Console.WriteLine("Kelimeler son "+kelimelerSon.Count);
+            Console.WriteLine(sayi);
+
+            for (int i = 0; i < kelimelerSon.Count; i++)
+            {
+               int kelimeFrekans = 1;
+                for (int j = i + 1; j < kelimelerSon.Count; j++)
+                {
+                    if (String.Equals(kelimelerSon[i], kelimelerSon[j]))
+                    {
+                        kelimeFrekans++;
+                        kelimelerSon.RemoveAt(j);
+                        j -= 1;
+                    }
+                }
+                WordAndFreq yeni = new WordAndFreq();
+                yeni.Word = kelimelerSon[i];
+                yeni.Frequency = kelimeFrekans;
+                KelimeSayilari.Add(yeni);
+            }
+
+
+            foreach (var item in kelimelerSon)
+            {
                 Console.WriteLine(item);
             }
 
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------KOK BULUNMUŞ-----------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-
-            //KokBul(new Iveonik.Stemmers.EnglishStemmer(), kelimeler);
-
-
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-            //Console.WriteLine("-----------------------------------------------------------------------------------");
-
-            //foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//html//text()"))
-            //{
-            //    if (!string.IsNullOrEmpty(node.InnerText) && !string.IsNullOrWhiteSpace(node.InnerText))
-            //        Console.WriteLine("text=" + node.InnerText);
-
-            //}
-
-            //List<int> sayilar = new List<int>();
-            //sayilar.Add(1);
-            //sayilar.Add(2);
-            //sayilar.Add(3);
-            //sayilar.Add(4);
-            //sayilar.Add(4);
-            //sayilar.Add(5);
-            //for (int i = 0; i < sayilar.Count; i++)
-            //{
-            //    if (sayilar[i] == 4)
-            //    {
-            //        sayilar.RemoveAt(i);
-            //        i -= 1;
-            //    }
-
-
-            //}
-            //foreach (var item in sayilar)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //string isim = "ABCDEF";
-            //string isim2= isim.Remove(2);
-            //Console.WriteLine(isim2);
-            
+            Console.WriteLine("----------------Frekans Listesi--------------------------------");
+          
+            foreach (var item in KelimeSayilari)
+            {
+                Console.WriteLine(item.Word+" "+item.Frequency);
+            }
             Console.ReadLine();
         }
 
 
-        //private static void KokBul(IStemmer stemmer, List<string> words)
-        //{
-        //    Console.WriteLine("Stemmer: " + stemmer);
-        //    foreach (string word in words)
-        //    {
-        //        Console.WriteLine(word + " --> " + stemmer.Stem(word));
-        //    }
-        //}
-
+      
 
 
 
